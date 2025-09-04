1d197e50bf9505d741e692041d5973584c73dd45
nwG1 <RGFalconer@Student.FullSail.edu>
2025-09-04 01:45:14 -0400

    Rule Explanation

--- EchoGrid.cpp
+++ EchoGrid.cpp
@@ -45,3 +45,26 @@ int main() {
 	std::cout << " The rules are different here. Victory requires luck, guts, and strategy.\n\n";
 	std::cout << " Press Enter to see the rules...";
 	std::cin.get();
+
+	// --- Rule Explanation ---
+	clearScreen();
+	setConsoleColor(COLOR_YELLOW);
+	std::cout << "================================ R U L E S ================================\n\n";
+	setConsoleColor(COLOR_WHITE);
+	std::cout << " 1. To start, both players roll a die. The highest roller gets the first turn.\n\n";
+	std::cout << " 2. On your turn, you will first toss a coin.\n";
+	std::cout << "    - If you WIN the toss, you get a POWER TURN!\n";
+	std::cout << "      You can either [Place] your mark on an EMPTY square OR...\n";
+	std::cout << "      You can attempt to [Conquer] a square already held by your OPPONENT!\n\n";
+	std::cout << "    - If you LOSE the toss, you have a NORMAL TURN.\n";
+	std::cout << "      You can only [Place] your mark on an EMPTY square.\n\n";
+	setConsoleColor(COLOR_RED);
+	std::cout << " >> WARNING: If you attempt to [Conquer] a square that isn't held by the\n";
+	std::cout << " >>          opponent (e.g., an empty square), YOU FORFEIT YOUR TURN!\n\n";
+	setConsoleColor(COLOR_WHITE);
+	std::cout << " 3. The first player to get three of their marks in a row wins!\n\n";
+	setConsoleColor(COLOR_YELLOW);
+	std::cout << "===========================================================================\n\n";
+	std::cout << "Press Enter to begin the showdown...";
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	std::cin.get();
